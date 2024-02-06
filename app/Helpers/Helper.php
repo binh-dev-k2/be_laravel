7270a25beef06e8fe4f404e28dbdc3f2dc65d673
Tran Binh <binhcoder02@gmail.com>
2024-02-06 13:45:26 +0700

    update response charset

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -3,18 +3,36 @@
 
 function successResponse($code, $data = [])
 {
-    return response()->xml([
-        'status' => "Thành công",
-        'data' => $data,
-        'code' => $code,
-    ], 200);
+    // return response()->xml($data, $status = 200, array $headers = [], $xmlRoot = 'response', $encoding = null);
+    return response()->xml(
+        [
+            'status' => "Thành công",
+            'data' => $data,
+            'code' => $code,
+        ],
+        200,
+        [
+            'Content-Type' => 'application/xml',
+        ],
+        'response',
+        'utf-8'
+    );
 }
 
 function errorResponse($code, $data = [])
 {
-    return response()->xml([
-        'status' => "Thất bại",
-        'data' => $data,
-        'code' => $code,
-    ], 200);
+    // return response()->xml($data, $status = 200, array $headers = [], $xmlRoot = 'response', $encoding = null);
+    return response()->xml(
+        [
+            'status' => "Thất bại",
+            'data' => $data,
+            'code' => $code,
+        ],
+        200,
+        [
+            'Content-Type' => 'application/xml',
+        ],
+        'response',
+        'utf-8'
+    );
 }
