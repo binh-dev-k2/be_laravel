4c3e074a105967441f879678ad62d277ee88ba28
Tran Binh <binhcoder02@gmail.com>
2024-02-14 22:57:42 +0700

    update

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -36,3 +36,27 @@ function xmlErrorResponse($code, $data = [])
         'utf-8'
     );
 }
+
+function successResponse($code, $data = [])
+{
+    return response()->json(
+        [
+            'status' => "Thành công",
+            'data' => $data,
+            'code' => $code,
+        ],
+        200
+    );
+}
+
+function errorResponse($code, $data = [])
+{
+    return response()->json(
+        [
+            'status' => "Thất bại",
+            'data' => $data,
+            'code' => $code,
+        ],
+        200
+    );
+}
