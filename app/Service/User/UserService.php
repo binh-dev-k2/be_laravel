1185d3f755d265fd14e5914937cb3418411d8920
Tran Binh <binhcoder02@gmail.com>
2024-02-20 13:50:57 +0700

    update otp

---
+++ app/Service/User/UserService.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserService {
+
+    public function findUserByEmail($email) {
+        return User::where('email', $email)->first();
+    }
+
+    public function createUser($data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'uuid' => Str::uuid(),
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+}
