8997e4d395fcc190ce838b888f9c17b8b5fb967a
Tran Binh <binhcoder02@gmail.com>
2024-03-01 20:04:36 +0700

    KA-5 | update invite code

--- app/Services/Couple/CoupleInvitationService.php
+++ app/Services/Couple/CoupleInvitationService.php
@@ -3,10 +3,21 @@
 namespace App\Services\Couple;
 
 use App\Models\Couple\CoupleInvitation;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CoupleInvitationService
 {
+    protected $coupleService;
+
+    public function __construct(CoupleService $coupleService)
+    {
+        $this->coupleService = $coupleService;
+    }
+
     public function sendInvite($fromUuid, $toUuid)
     {
         return CoupleInvitation::create([
@@ -25,23 +36,41 @@ class CoupleInvitationService
 
     public function updateInvitation($userUuid, $status)
     {
-        $currentUserUuid = Auth::user()->uuid;
-        $invitation = CoupleInvitation::query()
-            ->where('from_uuid', $currentUserUuid)
-            ->where('status', CoupleInvitation::STATUS_PENDING);
+        try {
+            DB::beginTransaction();
+            $currentUserUuid = Auth::user()->uuid;
+            $invitation = CoupleInvitation::query()
+                ->where('from_uuid', $currentUserUuid)
+                ->where('status', CoupleInvitation::STATUS_PENDING);
 
-        if ($currentUserUuid == $userUuid) {
-            $invitation->first();
-        } else {
-            $invitation->where('to_uuid', $userUuid)->first();
+            if ($currentUserUuid == $userUuid) {
+                $invitation->first();
+            } else {
+                $invitation->where('to_uuid', $userUuid)->first();
+            }
+
+            if (!$invitation) {
+                return false;
+            }
+
+            $invitation->update(['status' => $status]);
+            if ($status == CoupleInvitation::STATUS_ACCEPTED) {
+                $couple = $this->coupleService->createCouple($currentUserUuid, $userUuid);
+                DB::table('couple_timelines')->insert([
+                    'couple_uuid' => $couple['uuid'],
+                    'start_date' => Carbon::now()->format("Y-m-d")
+                ]);
+            }
+
+            if ($currentUserUuid != $userUuid) {
+                // Gui thong bao cho current user
+            }
+
+            DB::commit();
+            return true;
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::error(date("Y-m-d H:i:s") . ": " . $e->getMessage());
         }
-        if (!$invitation) {
-            return false;
-        }
-        $invitation->update(['status' => $status]);
-        if ($currentUserUuid == $userUuid) {
-            // Gui thong bao cho user o day
-        }
-        return true;
     }
 }
